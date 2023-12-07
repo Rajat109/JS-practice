6826bc13def291d95f149db89446e7b8e21a774b
Rajat Agarwal <63335782+Rajat109@users.noreply.github.com>
2023-12-07 08:17:27 +0000

    memory concepts

--- basics/datatype-summary.js
+++ basics/datatype-summary.js
@@ -7,10 +7,10 @@
 const id = Symbol(123)
 const id2 = Symbol('123')
 
-console.log(id); 
-console.log(id2); 
-console.log(id == id2);
-console.log(id === id2);
+// console.log(id); 
+// console.log(id2); 
+// console.log(id == id2);
+// console.log(id === id2);
 
 const bigNumber = 213221456886645n // to make a number bigInt we add 'n' at the end
 
@@ -35,5 +35,40 @@ const myFunction = function(){
     console.log("Hello Rajat")
 }
 
-console.log(typeof(id));
+//console.log(typeof(id));
+
+
+
+// ************** Memories ********************
+
+// Stack memory(Primitive), Heap memory(Non-primitve)
+
+// Example of primitive datatype and stack memory
+let myEmail = "rajat@gmail.com"
+
+let myAnotherEmail = myEmail
+
+myAnotherEmail = "rajat111@gmail.com"
+
+console.log(myEmail); // output=> rajat@gmail.com
+console.log(myAnotherEmail);// output=> rajat111@gmail.com
+
+// in Primitive data types a copy of data is sent and on changing the data inside myAnotherEmail the data inside myEmail does not change
+
+// Example of Non-Primitive datatype and heap memory
+
+let userOne = {
+    email:"user1@gmail.com",
+    upi:"user1@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user2@gmail.com"
+
+console.log(userOne.email);// output=> user2@gmail.com
+console.log(userTwo.email);// output=> user2@gmail.com
+
+// In non primitive datatypes heap memory is used and which is referenced when used by another variable
+// so when we changed data inside userTwo.email tha data inside userOne.email also got changed
 
