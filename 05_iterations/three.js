d03b1613c620b258137e464b8b7a93f622b81e87
Rajat Agarwal <63335782+Rajat109@users.noreply.github.com>
2023-12-12 09:14:25 +0000

    for-in loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,17 @@
+
+const obj = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby"
+}
+
+for (const key in obj) {
+    console.log(`${key} shortform is for ${obj[key]}`);
+    }
+
+const languages = ["js", "cpp", "python"]
+
+for (const key in languages) {
+    console.log(languages[key]);    
+    }
+
