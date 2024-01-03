cedb3c8a63f2a514dc8eb233f7e7fc63dd032cce
Rajat Agarwal <63335782+Rajat109@users.noreply.github.com>
2024-01-03 15:17:26 +0000

    Project-3 DigitalClock done

---
+++ projects/03-didital-clock/script.js
@@ -0,0 +1,10 @@
+const clock = document.getElementById('clock');
+
+
+// console.log(date.toLocaleTimeString());
+
+setInterval(function(){
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString()
+    // console.log(date.toLocaleTimeString());
+}, 1000)
