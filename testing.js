fae3555a864acbff14deb2f5112e9c57d2d380d7
Rajat Agarwal <63335782+Rajat109@users.noreply.github.com>
2024-01-09 08:31:13 +0000

    async testing

--- testing.js
+++ testing.js
@@ -1,6 +1,14 @@
-const newObj = {
-    email: "rajat",
-    "name": "rajat@109"
-}
+// const newObj = {
+//     email: "rajat",
+//     "name": "rajat@109"
+// }
 
-console.log(newObj.hasOwnProperty("emai"));
+// console.log(newObj.hasOwnProperty("emai"));
+
+console.log('1')
+setTimeout(function(){
+    console.log('2')
+},0)
+console.log('3')
+console.log('4')
+console.log('5')
